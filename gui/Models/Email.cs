6d30713b303efd29700f03f565a3c275f202e9bb
karinaves <karinaves1991@gmail.com>
2017-09-10 22:40:51 +0300

    check3

--- gui/Models/Email.cs
+++ gui/Models/Email.cs
@@ -20,7 +20,7 @@ namespace gui.Models
               Body = "שלום, {0}" +
                   "\nישנה סדנא חדשה באזורכם. \n" +
                   "מעבר לעמוד השיבוץ: {1}"
-              }  //blabla
+              }  //blabladfdfdfdfdjhggjhjj
             },
             { Type.VolunteerInvite,
                new MessageStructure()
