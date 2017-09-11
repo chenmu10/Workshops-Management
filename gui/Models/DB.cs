e1db74d91410dfdc620eaab2e8cba8c71dcfb233
chen <chenmu@mail.mta.ac.il>
2017-09-11 13:27:11 +0300

    updated db.cs

--- gui/Models/DB.cs
+++ gui/Models/DB.cs
@@ -20,7 +20,7 @@ namespace gui
         public DB()
         {
         }
-        public string DatabaseName= "MMT_DB";
+        public string DatabaseName = "MMT_DB";
         public string Password { get; set; }
         private MySqlConnection connection = null;
         public MySqlConnection Connection
@@ -82,7 +82,7 @@ namespace gui
                 {
                     Volunteer volunteer = new Volunteer(dr);
                     volunteer.Volunteer_Area_Activity = new List<int>();
-                    query2= string.Format("SELECT * FROM VolunteerToAreas WHERE Volunteer_ID='{0}'", volunteer.Volunteer_ID);
+                    query2 = string.Format("SELECT * FROM VolunteerToAreas WHERE Volunteer_ID='{0}'", volunteer.Volunteer_ID);
                     DataTable dt2 = Select(query2);
                     if (dt2 != null)
                     {
@@ -92,7 +92,7 @@ namespace gui
                             volunteer.Volunteer_Area_Activity.Add(area);
                         }
                     }
-                  result.Add(volunteer);
+                    result.Add(volunteer);
                 }
             }
 
@@ -159,6 +159,7 @@ namespace gui
             }
             return result;
         }
+
         /// <summary>
         /// Insert new Volunteer - ID must of null , and all infomation correct
         /// True - if update succeded 
@@ -169,12 +170,12 @@ namespace gui
         public Boolean InsetNewVolunteer(Volunteer volunteer)
         {
             try
-            {           
-            query = string.Format(@"INSERT INTO Volunteer 
+            {
+                query = string.Format(@"INSERT INTO Volunteer 
                 VALUES(null,{0},'{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}',{10},False,{11});",
-                volunteer.Volunteer_Practice, volunteer.Volunteer_First_Name, volunteer.Volunteer_First_Name_Eng, volunteer.Volunteer_Last_Name,
-                volunteer.Volunteer_Last_Name_Eng,volunteer.Volunteer_Email, volunteer.Volunteer_phone, volunteer.Volunteer_Occupation, volunteer.Volunteer_Reference,
-                volunteer.Volunteer_Employer, volunteer.Volunteer_Number_Of_Activities, volunteer.Volunteer_prefer_traning_area); 
+                    volunteer.Volunteer_Practice, volunteer.Volunteer_First_Name, volunteer.Volunteer_First_Name_Eng, volunteer.Volunteer_Last_Name,
+                    volunteer.Volunteer_Last_Name_Eng, volunteer.Volunteer_Email, volunteer.Volunteer_phone, volunteer.Volunteer_Occupation, volunteer.Volunteer_Reference,
+                    volunteer.Volunteer_Employer, volunteer.Volunteer_Number_Of_Activities, volunteer.Volunteer_prefer_traning_area);
                 query += "SELECT Volunteer_ID FROM mmt_db.volunteer order by Volunteer_ID DESC LIMIT 1;";
                 int row = Insert(query);
                 if (row == 0) return false;
@@ -184,20 +185,20 @@ namespace gui
 
 
                 foreach (int Area in volunteer.Volunteer_Area_Activity)
-            {
-                query = string.Format(@"INSERT INTO VolunteerToAreas VALUES(null,{0},{1});", volunteer.Volunteer_ID, Area);
+                {
+                    query = string.Format(@"INSERT INTO VolunteerToAreas VALUES(null,{0},{1});", volunteer.Volunteer_ID, Area);
                     if (!Update(query)) return false;
+                }
             }
-            }
-            catch(Exception e)
+            catch (Exception e)
             {
                 return false;
             }
             return true;
-        }  
-        public Boolean UpdateVolunteerTraning(Volunteer volunteer , int new_status)
+        }
+        public Boolean UpdateVolunteerTraning(Volunteer volunteer, int new_status)
         {
-            query =string.Format(@"UPDATE Volunteer SET Volunteer_Practice = {0} WHERE Volunteer_ID = {1};",new_status,volunteer.Volunteer_ID);
+            query = string.Format(@"UPDATE Volunteer SET Volunteer_Practice = {0} WHERE Volunteer_ID = {1};", new_status, volunteer.Volunteer_ID);
             return Update(query);
         }
         /// <summary>
@@ -210,6 +211,22 @@ namespace gui
             query = string.Format(@"DELETE FROM Volunteer WHERE Volunteer_ID = {0};", volunteer.Volunteer_ID);
             return Update(query);
         }
+
+        public Volunteer GetVolunteerByID(int ID)
+        {
+            Volunteer result = new Volunteer();
+            query = string.Format("SELECT * FROM mmt_db.volunteer WHERE Volunteer_ID ={0}", ID);
+            DataTable dt = Select(query);
+            if (dt != null)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    return new Volunteer(dr);
+                }
+            }
+            return result;
+        }
+
         #endregion
 
         #region WorkShop
@@ -227,7 +244,51 @@ namespace gui
             }
             return result;
         }
+        public bool updateSchoolWorkshopVolunteer(int schoolWorkShopID, int iD1, int iD2, int iD3, string ride1, string ride2, string ride3)
+        {
+            string i1, i2, i3;
+            if (iD1 == 0) i1 = "NULL";
+            else
+            {
+                i1 = iD1.ToString();
+                query = string.Format(@"
+                 INSERT INTO School_WorkShop_Ride VALUES({0},{1},'{2}');
+                ", schoolWorkShopID, iD1, ride1);
+                Insert(query);
+            }
+            if (iD2 == 0) i2 = "NULL";
+            else
+            {
+                i2 = iD2.ToString();
+                query = string.Format(@"
+                 INSERT INTO School_WorkShop_Ride VALUES({0},{1},'{2}');
+                ", schoolWorkShopID, iD2, ride2);
+                Insert(query);
+            }
+            if (iD3 == 0) i3 = "NULL";
+            else
+            {
+                i3 = iD3.ToString();
+                query = string.Format(@"
+                 INSERT INTO School_WorkShop_Ride VALUES({0},{1},'{2}');
+                ", schoolWorkShopID, iD3, ride3);
+                Insert(query);
+            }
 
+            query = string.Format(@"
+            UPDATE schoolworkshop SET 
+            WorkShop_Volunteer1={0}, WorkShop_Volunteer2={1}, WorkShop_Volunteer3={2} WHERE WorkShop_ID={3};
+                ", i1, i2, i3, schoolWorkShopID);
+            return Update(query);
+        }
+        public bool updateCompanyWorkshopSchoolAssign(string WorkshopID, School School, string estimatedParticipants, string comments)
+        {
+            query = string.Format(@"
+            UPDATE companyworkshop SET 
+            WorkShop_School_ID={0}, WorkShop_School_Comments={1}, WorkShop_Number_Of_StudentPredicted={2},WorkShop_Status={3} WHERE WorkShop_ID={4};
+                ", School, comments, estimatedParticipants, 4, WorkshopID);
+            return Update(query);
+        }
         public List<CompanyWorkshop> GetAllCompanyWorshops()
         {
             List<CompanyWorkshop> result = new List<CompanyWorkshop>();
@@ -257,7 +318,8 @@ namespace gui
             return result;
         }
 
-        public bool InsertNewSchoolWorkShop(SchoolWorkShop NewSchoolWorkShop) {
+        public bool InsertNewSchoolWorkShop(SchoolWorkShop NewSchoolWorkShop)
+        {
             bool result = true;
 
             /*INSERT INTO SchoolWorkShop VALUES(null,1,'2017-1-1 12:12:00','2017-1-1 12:12:00','2017-1-12 12:12:00',1,1,2,3,4,20,20,'להדליק את המזגן לפני','רכזת עמט מיכל','052-5782802','mci@gamil.com',false,1);*/
@@ -301,14 +363,14 @@ namespace gui
                 foreach (DataRow dr in dt.Rows)
                 {
 
-                    WorkshopJoin t = new WorkshopJoin(dr,2);
+                    WorkshopJoin t = new WorkshopJoin(dr, 2);
                     t.Is_company = false;
                     result.Add(t);
                 }
             }
             return result;
         }
- 
+
         public SchoolWorkShop GetSchoolWorkShopByJoinID(int ID)
         {
             SchoolWorkShop result = new SchoolWorkShop();
@@ -323,7 +385,20 @@ namespace gui
             }
             return result;
         }
-
+        public CompanyWorkshop getCompanyWorkshopByID(int ID)
+        {
+            CompanyWorkshop result = new CompanyWorkshop();
+            query = string.Format("SELECT * FROM mmt_db.companyworkshop WHERE WorkShop_ID ={0}", ID);
+            DataTable dt = Select(query);
+            if (dt != null)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    return new CompanyWorkshop(dr);
+                }
+            }
+            return result;
+        }
         public WorkshopJoin GetJoinWorkShopByID(int ID)
         {
             WorkshopJoin SchoolWorkshops = new WorkshopJoin();
@@ -352,7 +427,7 @@ namespace gui
             }
             return sc;
         }
-        public bool SchoolWorkShopUpdateDate(int SchoolID,int selectDate)
+        public bool SchoolWorkShopUpdateDate(int SchoolID, int selectDate)
         {
             query = string.Format(@"UPDATE schoolworkshop SET WorkShop_SelectedDate = {0} WHERE WorkShop_ID = {1};", selectDate, SchoolID);
             return Update(query);
@@ -395,6 +470,20 @@ namespace gui
             return result;
 
         }
+        public string getVolunteerSchoolRide(int volunteer_ID, int workshopId)
+        {
+            string result = "";
+            query = string.Format("SELECT School_WorkShop_Ride_Comment FROM mmt_db.School_WorkShop_Ride where School_WorkShop_Ride_ID = {0} && School_WorkShop_Ride_Volunteer ={1}", workshopId, volunteer_ID);
+            DataTable dt = Select(query);
+            if (dt != null)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    return dr["School_WorkShop_Ride_Comment"].ToString();
+                }
+            }
+            return result;
+        }
         #endregion
 
         #region Manager
@@ -416,7 +505,7 @@ namespace gui
         //    return result;
         //}
 
-        public Dictionary<int,string> GetAreaActivity()
+        public Dictionary<int, string> GetAreaActivity()
         {
             Dictionary<int, string> result = new Dictionary<int, string>();
             query = string.Format(@"SELECT * FROM mmt_db.area_activity; ");
@@ -430,6 +519,9 @@ namespace gui
             }
             return result;
         }
+
+
+
         public Dictionary<int, string> GetVolunteerStatus()
         {
             Dictionary<int, string> result = new Dictionary<int, string>();
@@ -453,8 +545,8 @@ namespace gui
             {
                 query = string.Format(@"INSERT INTO School VALUES(null,{0},'{1}','{2}','{3}',{4},'{5}','{6}','{7}','{8}','{9}','{10}');",
                 school.School_Serial_Number, school.School_Name, school.School_Address, school.School_City, school.School_Area, school.School_Contact_Name, school.Scool_Contact_Phone,
-                school.School_Contact_Email, school.School_Supervisor_Name, school.School_Supervisor_Phone,school.School_Parking_Info
-                    ); 
+                school.School_Contact_Email, school.School_Supervisor_Name, school.School_Supervisor_Phone, school.School_Parking_Info
+                    );
                 query += "SELECT School_ID FROM mmt_db.School order by School_ID DESC LIMIT 1;";
                 int row = Insert(query);
                 if (row == 0) return false;
@@ -472,7 +564,7 @@ namespace gui
         public School GetSchoolByID(int ID)
         {
             School result = new School();
-            query = string.Format("SELECT * FROM School WHERE School_ID={0}",ID);
+            query = string.Format("SELECT * FROM School WHERE School_ID={0}", ID);
             DataTable dt = Select(query);
             if (dt != null)
             {
@@ -482,7 +574,7 @@ namespace gui
                 }
             }
             return result;
-        }       
+        }
         #endregion
 
         #region Company
@@ -530,8 +622,8 @@ namespace gui
         {
             //TODO 
             return true;
-           // query = string.Format(@"UPDATE Company SET Volunteer_Practice = {0} WHERE Volunteer_ID = {1};", );
-           // return Update(query);
+            // query = string.Format(@"UPDATE Company SET Volunteer_Practice = {0} WHERE Volunteer_ID = {1};", );
+            // return Update(query);
         }
         public Boolean DeleteCompany(Company company)
         {
@@ -624,13 +716,13 @@ namespace gui
             {
                 try
                 {
-                    
+
                     //Create Command
                     MySqlCommand cmd = new MySqlCommand(query, connection);
 
                     //Create a data reader and Execute the command
-                    result=Convert.ToInt32(cmd.ExecuteScalar());
-                   // result = cmd.ExecuteNonQuery();
+                    result = Convert.ToInt32(cmd.ExecuteScalar());
+                    // result = cmd.ExecuteNonQuery();
                 }
                 catch (MySqlException e)
                 {
